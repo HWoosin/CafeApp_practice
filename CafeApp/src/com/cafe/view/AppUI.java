4d79e38908b9cd639130ab3cbaecb262a5829e5f
woosin <ws1340cc@naver.com>
2023-04-27 18:09:50 +0900

    로그인을 구현하였습니다.  230427

--- CafeApp/src/com/cafe/view/AppUI.java
+++ CafeApp/src/com/cafe/view/AppUI.java
@@ -6,18 +6,25 @@ public class AppUI {
 		System.out.println("\n=========== CAFE ORDER SYSTEM ===========");
 		System.out.println("♠♠♠ 1. 로그인");
 		System.out.println("♠♠♠ 2. 회원가입");
-		System.out.println("♤♤♤ 3. 비회원 주문");
 		System.out.println("♠♠♠ 4. 프로그램 종료");
 		System.out.println("-----------------------------------------");
 		System.out.print("☞ ");
 	}
 	
 	public static void UserMenu() {
-		System.out.println("\n=========== CAFE ORDER SYSTEM ===========");
-		System.out.println("♠♠♠ 1. 상품조회");
-		System.out.println("♠♠♠ 2. 포인트조회");
-		System.out.println("♠♠♠ 3. 주문조회");
-		System.out.println("♠♠♠ 4. 나가기");
+		System.out.println("\n=========== USER MENU ===========");
+		System.out.println("♥♥♥ 1. 주문하기");
+		System.out.println("♥♥♥ 2. 포인트조회");
+		System.out.println("♥♥♥ 3. 주문조회");
+		System.out.println("♥♥♥ 4. 메인으로");
+		System.out.println("-----------------------------------------");
+		System.out.print("☞ ");
+	}
+	
+	public static void OrderMenu() {
+		System.out.println("\n=========== ORDER MENU ===========");
+		System.out.println("★★★ 1. 메뉴선택");
+		System.out.println("★★★ 2. 뒤로가기");
 		System.out.println("-----------------------------------------");
 		System.out.print("☞ ");
 	}
